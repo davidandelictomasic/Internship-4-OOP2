0c3b28d30f1e20fd99ab2402a37d15267d4d9793
davidandelictomasic <david.andelic.tomasic@gmail.com>
2025-11-28 13:19:00 +0100

    Add Application success response types

---
+++ UserManagement.Application/Common/Model/SuccessPostResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace UserManagement.Application.Common.Model
+{
+    public class SuccessPostResponse
+    {
+        public int? Id { get; init; }
+        public SuccessPostResponse(int? id)
+        {
+            Id = id;
+        }
+        public SuccessPostResponse()
+        {
+        }
+    }
+}
