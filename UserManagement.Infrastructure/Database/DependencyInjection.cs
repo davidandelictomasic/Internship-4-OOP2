b29fb82a4b430a426e5b9ff1379d9441233baaab
davidandelictomasic <david.andelic.tomasic@gmail.com>
2025-11-28 18:35:58 +0100

    Add dependency injection

---
+++ UserManagement.Infrastructure/Database/DependencyInjection.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using UserManagement.Domain.Persistence.Common;
+using UserManagement.Domain.Persistence.Users;
+using UserManagement.Infrastructure.Dapper;
+using UserManagement.Infrastructure.Repositories.Common;
+using UserManagement.Infrastructure.Repositories.Users;
+
+namespace UserManagement.Infrastructure.Database
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            AddDatabase(services, configuration);
+
+            return services;
+        }
+
+        private static void AddDatabase(IServiceCollection services, IConfiguration configuration)
+        {
+            string? connectionString = configuration.GetConnectionString("Database");
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                throw new ArgumentNullException(nameof(connectionString));
+            }
+            services.AddScoped<DbContext>(provider => provider.GetRequiredService<ApplicationDbContext>());
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                    options.UseNpgsql(connectionString));
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IUserUnitOfWork, UserUnitOfWork>();
+
+            services.AddSingleton<IDapperManager>(sp =>
+            {
+                var config = sp.GetRequiredService<IConfiguration>();
+                string cs = config.GetConnectionString("Database");
+                return new DapperManager(cs);
+            });
+
+        }
+    }
+}
