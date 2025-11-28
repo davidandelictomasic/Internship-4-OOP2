0325e2b9ef743c3049fdc112946b0270a72729b6
davidandelictomasic <david.andelic.tomasic@gmail.com>
2025-11-28 13:29:03 +0100

    Add Application result

---
+++ UserManagement.Application/Common/Validation/ValidationResultItem.cs
@@ -0,0 +1,25 @@
+﻿
+using UserManagement.Domain.Common.Validation;
+using UserManagement.Domain.Enumumerations.Validation;
+
+namespace UserManagement.Application.Common.Validation
+{
+    public class ValidationResultItem
+    {
+        public ValidationSeverity ValidationSeverity { get; init; }
+        public ValidationType ValidationType { get; init; }
+        public string Code { get; set; }
+        public string Message { get; set; }
+        
+        public static ValidationResultItem FromValidationItem(ValidationItem item)
+        {
+            return new ValidationResultItem
+            {
+                ValidationSeverity = item.ValidationSeverity,
+                ValidationType = item.ValidationType,
+                Code = item.Code,
+                Message = item.Message
+            };
+        } 
+    }
+}
