b5a7febef7a49e3f881438982efe2567bb33e715
davidandelictomasic <david.andelic.tomasic@gmail.com>
2025-11-28 17:35:25 +0100

    Add application database context

---
+++ UserManagement.Infrastructure/Database/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserManagement.Domain.Entities.Companies;
+using UserManagement.Domain.Entities.Users;
+
+namespace UserManagement.Infrastructure.Database
+{
+    internal class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Company> Companies { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+            modelBuilder.HasDefaultSchema("public");
+        }
+    }
+}
