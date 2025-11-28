177671117a887c9dc358971cef4a15ab2ab6b208
davidandelictomasic <david.andelic.tomasic@gmail.com>
2025-11-28 17:51:56 +0100

    Add Infrastructure repository/unit of work

---
+++ UserManagement.Infrastructure/Repositories/Common/Repository.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserManagement.Domain.Common.Model;
+using UserManagement.Domain.Persistence.Common;
+
+namespace UserManagement.Infrastructure.Repositories.Common
+{
+    public class Repository<TEntity, TId> : IRepository<TEntity, TId> where TEntity : class
+    {
+        private readonly DbContext _context;
+        private readonly DbSet<TEntity> _dbSet;
+
+
+        public async Task InsertAsync(TEntity entity)
+        {
+            await _dbSet.AddAsync(entity);
+
+        }
+
+        public void Update(TEntity entity)
+        {
+            _dbSet.Update(entity);
+        }
+        public Task<GetAllResponse<TEntity>> Get()
+        {
+            return _dbSet.ToListAsync().ContinueWith(task => new GetAllResponse<TEntity>
+            {
+                Values = task.Result
+            });
+        }
+        public void Delete(TEntity? entity)
+        {
+            if (entity != null)
+            {
+                _dbSet.Remove(entity);
+            }
+        }
+
+        public async Task DeleteAsync(TId id)
+        {
+            var entity = await _dbSet.FindAsync(id);
+            if (entity != null)
+            {
+                _dbSet.Remove(entity);
+            }
+        }
+    }
+}
---
+++ UserManagement.Infrastructure/UnitOfWork.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using UserManagement.Domain.Persistence.Common;
+using UserManagement.Infrastructure.Database;
+
+namespace UserManagement.Infrastructure
+{
+    public class UnitOfWork : IUnitOfWork
+
+    {
+        private readonly ApplicationDbContext _dbContext;
+        
+        public UnitOfWork(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+       
+        public async Task SaveAsync()
+        {
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
