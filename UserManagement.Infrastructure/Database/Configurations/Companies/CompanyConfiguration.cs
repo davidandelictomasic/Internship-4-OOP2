4835867bc5c7224d9c31f9146f45a6cad7cd60b3
davidandelictomasic <david.andelic.tomasic@gmail.com>
2025-11-28 17:31:47 +0100

    Add user/company table configurations

---
+++ UserManagement.Infrastructure/Database/Configurations/Companies/CompanyConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using UserManagement.Domain.Entities.Companies;
+
+namespace UserManagement.Infrastructure.Database.Configurations.Companies
+{
+    internal class CompanyConfiguration : IEntityTypeConfiguration<Company>
+
+    {
+        public void Configure(EntityTypeBuilder<Company> builder)
+        {
+            builder.ToTable("Companies");
+            builder.HasKey(c => c.ID);
+
+            builder.Property(c => c.ID).HasColumnName("Id");
+
+            builder.Property(c => c.Name).HasColumnName("name");
+            
+
+
+
+        }
+    }
+}
---
+++ UserManagement.Infrastructure/Database/Configurations/Users/UserConfiguration.cs
@@ -0,0 +1,31 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using UserManagement.Domain.Entities.Users;
+
+namespace UserManagement.Infrastructure.Database.Configurations.Users
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable("Users");
+            builder.HasKey(u => u.ID);
+
+            builder.Property(u => u.ID).HasColumnName("Id");
+
+            builder.Property(u => u.Name).HasColumnName("name");
+            builder.Property(u => u.Username).HasColumnName("username");
+            builder.Property(u => u.Email).HasColumnName("email");
+            builder.Property(u => u.AddressStreet).HasColumnName("address_street");
+            builder.Property(u => u.AddressCity).HasColumnName("address_city");
+            builder.Property(u => u.GeoLocation).HasColumnName("geo_location");
+            builder.Property(u => u.Website).HasColumnName("website");
+            builder.Property(u => u.Password).HasColumnName("password");
+           
+
+
+    }
+    }
+}
