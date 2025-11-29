a819ad043c7c8d651d573ec4ae640d53d7685f72
davidandelictomasic <david.andelic.tomasic@gmail.com>
2025-11-29 19:19:26 +0100

    Add external API integration for users

---
+++ UserManagement.Application/Common/Interfaces/IExternalUserApi.cs
@@ -0,0 +1,10 @@
+﻿using UserManagement.Application.DTOs.Users;
+
+namespace UserManagement.Application.Common.Interfaces
+{
+    public interface IExternalUserApi
+    {
+        Task<IEnumerable<ExternalUserDto>> GetUsersAsync();
+    }
+
+}
---
+++ UserManagement.Application/DTOs/Users/ExternalUserDtocs.cs
@@ -0,0 +1,14 @@
+﻿namespace UserManagement.Application.DTOs.Users
+{
+    public class ExternalUserDto
+    {
+        public string Name { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string AddressStreet { get; set; }
+        public string AddressCity { get; set; }
+        public decimal GeoLat { get; set; }
+        public decimal GeoLng { get; set; }
+        public string? Website { get; set; }
+    }
+}
